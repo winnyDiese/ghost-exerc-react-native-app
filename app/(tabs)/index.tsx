af0026c2b6b66a77375a6c7d829370ee6cd38c86
winnyDiese <winnydiese@gmail.com>
2025-01-12 07:40:29 +0100

    create a text, and et view...

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,24 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>UI</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>This text is black !</Text>
     </View>
   );
 }
 
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'yellow'
+  },
+  text:{
+    color: 'black',
+    fontSize: 22
+  }
+})
