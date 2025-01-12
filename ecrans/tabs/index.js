8b7ffa200ef63c6b6fe36dc28b7308dead6c0186
winnyDiese <winnydiese@gmail.com>
2025-01-12 16:34:36 +0100

    create file index in ecrans/tabs/..

---
+++ ecrans/tabs/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const BootomTabs = () => {
+  return (
+    <View>
+      <Text>BootomTabs</Text>
+    </View>
+  )
+}
+
+export default BootomTabs
