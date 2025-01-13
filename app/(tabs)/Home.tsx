00b84a05eaa3b5fa63d01c3fc47302752873aa0c
winnyDiese <winnydiese@gmail.com>
2025-01-13 19:53:26 +0100

    finish ma first page in react native...

--- app/(tabs)/Home.tsx
+++ app/(tabs)/Home.tsx
@@ -1,10 +1,12 @@
-import { View, Text, StyleSheet, ScrollView } from 'react-native'
+import { View, Text, StyleSheet, ScrollView, Image, TextInput } from 'react-native'
 import React from 'react'
 
 const Home = () => {
   return (
     <ScrollView contentContainerStyle={styles.container}>
         <View>
+            <Text style={styles.title}>My App</Text>
+
             <Text style={styles.text}>
                 Home to tabs
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
@@ -12,6 +14,59 @@ const Home = () => {
                 veritatis porro dicta ea aperiam sit dolor quam tenetur, 
                 harum suscipit dolores deleniti eveniet animi.
             </Text>
+
+            <View>
+                <Image
+                    source={{
+                    uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+                    }}
+                    style={{width: 200, height: 200}}
+                />
+
+                <Text style={styles.text}>
+                    Home to tabs
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                    Molestiae ullam similique quia fuga tempore, officiis 
+                    veritatis porro dicta ea aperiam sit dolor quam tenetur, 
+                    harum suscipit dolores deleniti eveniet animi.
+                </Text>
+
+                <TextInput
+                    style={{
+                        height: 40,
+                        borderColor: 'gray',
+                        borderWidth: 1,
+                        marginBottom: 10, // Ajoute une marge de 10 unités en bas
+
+                    }}
+                    defaultValue='You can type in me !'
+                />
+            </View>
+
+            <Text style={styles.text}>
+                Home to tabs
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                Molestiae ullam similique quia fuga tempore, officiis 
+                veritatis porro dicta ea aperiam sit dolor quam tenetur, 
+                harum suscipit dolores deleniti eveniet animi.
+            </Text>
+
+            <Text style={styles.text}>
+                Home to tabs
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                Molestiae ullam similique quia fuga tempore, officiis 
+                veritatis porro dicta ea aperiam sit dolor quam tenetur, 
+                harum suscipit dolores deleniti eveniet animi.
+            </Text>
+
+            <Text style={styles.text}>
+                Home to tabs
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                Molestiae ullam similique quia fuga tempore, officiis 
+                veritatis porro dicta ea aperiam sit dolor quam tenetur, 
+                harum suscipit dolores deleniti eveniet animi.
+            </Text>
+
         </View>
     </ScrollView>
   )
@@ -21,13 +76,19 @@ export default Home
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    // flex: 1,
+    
     backgroundColor: 'yellow'
   },
   text:{
     color: 'black',
+    marginBottom: 30, 
     fontSize: 22
-  }
+  },
+  title: {
+    color: 'black',
+    fontSize: 40, // Taille raisonnable pour des écrans de tailles variées
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
 })
