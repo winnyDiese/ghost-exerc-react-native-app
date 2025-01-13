1aa2ea3dfbc3703295d51ac0d00073b06c173300
winnyDiese <winnydiese@gmail.com>
2025-01-13 12:34:38 +0100

    calling ecran.home.index in route directory..

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import {createNativeStackNavigator} from "@react-navigation/native-stack"
 import HomeScreen from '@/app/(tabs)';
+import Home from "../ecrans/Home"
 
 const Stack = createNativeStackNavigator()
 
@@ -10,7 +11,7 @@ const Routes = () => {
   return (
     <NavigationContainer>
         <Stack.Navigator>
-            <Stack.Screen name='Home' component={HomeScreen}/>
+            <Stack.Screen name='Home' component={Home}/>
         </Stack.Navigator>
     </NavigationContainer>
   )
