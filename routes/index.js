0215c04590c8f27b7565d23eb868cb9059708ca3
winnyDiese <winnydiese@gmail.com>
2025-01-13 12:21:50 +0100

    lkjb

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import {createNativeStackNavigator} from "@react-navigation/native-stack"
+import HomeScreen from '@/app/(tabs)';
+
+const Stack = createNativeStackNavigator()
 
 const Routes = () => {
   return (
-    <View>
-      <Text>index</Text>
-    </View>
   )
 }
 
