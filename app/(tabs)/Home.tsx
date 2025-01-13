e5099e460e11ec8c0b6258ae2b37c6122099bab4
winnyDiese <winnydiese@gmail.com>
2025-01-13 19:14:37 +0100

    lkjn

--- app/(tabs)/Home.tsx
+++ app/(tabs)/Home.tsx
@@ -1,12 +1,25 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home to tabs</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Home to tabs</Text>
     </View>
   )
 }
 
 export default Home
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'yellow'
+  },
+  text:{
+    color: 'black',
+    fontSize: 22
+  }
+})
