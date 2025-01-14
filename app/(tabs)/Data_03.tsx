a2ad61e56d75e4ed348f4aefc04d3dedaeda1c12
winnyDiese <winnydiese@gmail.com>
2025-01-14 14:27:35 +0100

    typing page data...

--- app/(tabs)/Data_03.tsx
+++ app/(tabs)/Data_03.tsx
@@ -1,10 +1,26 @@
 import {FlatList, StyleSheet, Text, View} from 'react-native';
+import renderer from 'react-test-renderer';
 
 const Data = () => {
 
     return (
         <View >
-            <FlatList>
+            <FlatList
+                data={[
+                    {key: 'Devin'},
+                    {key: 'Dan'},
+                    {key: 'Dominic'},
+                    {key: 'Jackson'},
+                    {key: 'James'},
+                    {key: 'Joel'},
+                    {key: 'John'},
+                    {key: 'Jillian'},
+                    {key: 'Jimmy'},
+                    {key: 'Julie'},
+                ]}
+
+                renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+            >
 
             </FlatList>
         </View>
