66153e88066010c8acb19ad6a3993b3a23e92d5b
winnyDiese <winnydiese@gmail.com>
2025-01-14 16:25:05 +0100

    boiubyv

--- app/(tabs)/Data_03.tsx
+++ app/(tabs)/Data_03.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-import {FlatList, StyleSheet, Text, View} from 'react-native';
+import {FlatList, SectionList, StyleSheet, Text, View} from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 import renderer from 'react-test-renderer';
 
 const Data = () => {
@@ -18,7 +19,9 @@ const Data = () => {
     ])
 
     return (
+        // <ScrollView
         <View style={{padding: 20, backgroundColor:'white'}}>
+
             <Text style={{fontSize: 30, fontWeight: 'bold'}}>First list</Text>
             <FlatList
                 style={{marginBottom:20}}
@@ -40,7 +43,7 @@ const Data = () => {
 
             </FlatList>
 
-            <Text style={{fontSize: 30, fontWeight: 'bold', marginBottom: 30}}>Custom first List</Text>
+            <Text style={{fontSize: 30, fontWeight: 'bold', marginBottom: 20}}>Custom first List</Text>
             <FlatList
                 data={data}
                 keyExtractor={(item, index) => index.toString()}
@@ -50,7 +53,32 @@ const Data = () => {
             >
             </FlatList>
 
-            
+            <Text style={{fontSize: 30, fontWeight: 'bold', marginBottom: 20}}>Second List</Text>
+            <SectionList
+                sections={[
+                    {title: 'D', data: ['Devin', 'Dan', 'Dominic']},
+                    {
+                        title: 'J',
+                        data: [
+                          'Jackson',
+                          'James',
+                          'Jillian',
+                          'Jimmy',
+                          'Joel',
+                          'John',
+                          'Julie',
+                        ],
+                    },
+                ]}
+
+                renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+                renderSectionHeader={({section}) => (
+                    <Text style={styles.sectionHeader2}>{section.title}</Text>
+                )}
+            >
+
+            </SectionList>
+
         </View>
     )
 }
@@ -66,5 +94,25 @@ const styles = StyleSheet.create({
         padding: 10,
         fontSize: 12,
         height: 30
-    }
+    },
+
+    // Second list
+    container2: {
+        flex: 1,
+        paddingTop: 22,
+      },
+      sectionHeader2: {
+        paddingTop: 2,
+        paddingLeft: 10,
+        paddingRight: 10,
+        paddingBottom: 2,
+        fontSize: 14,
+        fontWeight: 'bold',
+        backgroundColor: 'rgba(247,247,247,1.0)',
+      },
+      item2: {
+        padding: 10,
+        fontSize: 18,
+        height: 44,
+      },
 })
