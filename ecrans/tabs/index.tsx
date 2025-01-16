5ee737f1b28a956890ab4c28e6ffcce4c3080f01
winnyDiese <winnydiese@gmail.com>
2025-01-16 20:08:03 +0100

    lkjblkjb

--- ecrans/tabs/index.tsx
+++ ecrans/tabs/index.tsx
@@ -2,24 +2,25 @@ import { View, Text } from 'react-native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons'
 import React from 'react'
+import Home from '../Home'
 
 const Tab = createBottomTabNavigator()
 
 const BootomTabs = () => {
   return (
     <Tab.Navigator
-      initialRouteName="Feed"
+      initialRouteName="home"
       screenOptions={{
         tabBarActiveTintColor: "#e91e63"
       }}
     >
       <Tab.Screen
-        name='Feed'
-        component={feed}
+        name='home'
+        component={Home}
         options={{
           tabBarLabel: 'Home',
           tabBarIcon: ({color, size}) => {
-            <MaterialCommunityIcons name='home' color={color} size={size}
+            <MaterialCommunityIcons name='home' color={color} size={size} />
           }
         }}
       >
