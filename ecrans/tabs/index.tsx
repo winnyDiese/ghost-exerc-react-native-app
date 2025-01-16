237c9e840ac2075ec6d6cefa25d76fde5424a75f
winnyDiese <winnydiese@gmail.com>
2025-01-16 19:59:22 +0100

    lkjn

--- ecrans/tabs/index.tsx
+++ ecrans/tabs/index.tsx
@@ -1,11 +1,23 @@
 import { View, Text } from 'react-native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import React from 'react'
+
+const Tab = createBottomTabNavigator()
 
 const BootomTabs = () => {
   return (
-    <View>
-      <Text>BootomTabs page</Text>
-    </View>
+    <Tab.Navigator
+      initialRouteName="Feed"
+      screenOptions={{
+        tabBarActiveTintColor: "#e91e63"
+      }}
+    >
+      <Tab.Screen
+        name='Feed'
+      >
+
+      </Tab.Screen>
+    </Tab.Navigator>
   )
 }
 
