24d91c1e64edd95f9951b175a46bb7728095a12c
winnyDiese <winnydiese@gmail.com>
2025-01-17 16:45:52 +0100

    creating setting page in ecran directory...

---
+++ ecrans/Setting/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native"
+
+const Settings = () => {
+    return (
+        <View>
+            <Text>Setting page</Text>
+        </View>
+    )
+}
+
+export default Settings
