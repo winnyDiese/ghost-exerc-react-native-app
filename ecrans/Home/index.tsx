706e600a299c541f231c927e19ee480c78bbb6a4
winnyDiese <winnydiese@gmail.com>
2025-01-17 20:11:30 +0100

    iunpiun

--- ecrans/Home/index.tsx
+++ ecrans/Home/index.tsx
@@ -1,25 +1,18 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, ScrollView } from 'react-native'
 import React from 'react'
 
 const Home = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>This is the dashboard...</Text>
-    </View>
+    <ScrollView>
+      <View style={}>
+        <Text style={{color: 'white'}}>John Doe</Text>
+      </View>
+    </ScrollView>
   )
 }
 
 export default Home
 
 const styles = StyleSheet.create({
-  container: {
-    // flex: 1,
-    // justifyContent: 'center',
-    // alignItems: 'center',
-    backgroundColor: 'yellow'
-  },
-  text:{
-    color: 'black',
-    fontSize: 22
-  }
+  
 })
