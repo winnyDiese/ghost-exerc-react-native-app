7de69805f9f6232387fe2a6f1a1e3fa11f2748f1
winnyDiese <winnydiese@gmail.com>
2025-01-17 16:31:01 +0100

    jboub

---
+++ ecrans/Message/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native"
+
+const Messages = () => {
+
+    return (
+        <View>
+            <Text>Message page</Text>
+        </View>
+    )
+}
+
+export default Messages
