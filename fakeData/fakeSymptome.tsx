d4bb37bf585352792ff84c4fc7d02a112fc3e595
winnyDiese <winnydiese@gmail.com>
2025-01-21 21:03:31 +0100

    jbkhjb

---
+++ fakeData/fakeSymptome.tsx
@@ -0,0 +1,18 @@
+
+export const fakeSymptome = [
+    {
+        id: 1,
+        img: 'img1.png',
+        libelle: 'Mal à la tête'
+    },
+    {
+        id: 2,
+        img: 'img2.png',
+        libelle: 'Fièvre'
+    },
+    {
+        id: 3,
+        img: 'img3.png',
+        libelle: 'Diarhée'
+    },
+]
