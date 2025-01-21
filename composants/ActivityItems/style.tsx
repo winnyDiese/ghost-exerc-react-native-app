4591e2bd2155b76fdf931c0a7b49dc2508d3ea04
winnyDiese <winnydiese@gmail.com>
2025-01-21 20:40:00 +0100

    kjbuyboublhjkn

---
+++ composants/ActivityItems/style.tsx
@@ -0,0 +1,13 @@
+
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const style = () => {
+  return (
+    <View>
+      <Text>style</Text>
+    </View>
+  )
+}
+
+export default style
