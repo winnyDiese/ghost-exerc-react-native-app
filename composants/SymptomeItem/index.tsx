c2b1b02c32174456f01e2e0429f923c39e63c51c
winnyDiese <winnydiese@gmail.com>
2025-01-21 20:58:20 +0100

    kjbkjb

--- composants/SymptomeItem/index.tsx
+++ composants/SymptomeItem/index.tsx
@@ -1,2 +1,12 @@
 
-rnfe
+import { View, Text} from 'react-native'
+
+const SymptomeItem = ({item}) => {
+    return (
+        <View>
+            <Text>index</Text>
+        </View>
+    )
+}
+
+export default SymptomeItem
