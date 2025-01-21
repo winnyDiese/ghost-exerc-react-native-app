ea56c2fee913bfda9bc2fb328f8c42ba7e457394
winnyDiese <winnydiese@gmail.com>
2025-01-21 04:50:41 +0100

    ljbiujb

--- ecrans/Home/style.tsx
+++ ecrans/Home/style.tsx
@@ -2,7 +2,12 @@ import { StyleSheet } from "react-native"
 
 const dashboardStyles = StyleSheet.create({
     header: {
-        backgroundColor: 'blue'
+        // backgroundColor: 'blue',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingHorizontal: PADDING.horizontal,
+        paddingVertical: PADDING.vertical
     }
 })
 
