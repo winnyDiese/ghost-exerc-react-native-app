8eb5a3ebd300f80bad931fab321135cd8a5cf662
winnyDiese <winnydiese@gmail.com>
2025-01-21 06:01:34 +0100

    lkjbkjhb

--- ecrans/Home/index.tsx
+++ ecrans/Home/index.tsx
@@ -5,13 +5,24 @@ import dashboardStyles from './style';
 const Home = () => {
   return (
     <ScrollView>
+      {/*Debut du Header */}
       <View style={dashboardStyles.header}>
         <Text style={dashboardStyles.userName}>John Doe</Text>
         <Image 
           source={require('../../assets/images/icon.png')}
           style={dashboardStyles.userImage}
-          />
+        />
       </View>
+      {/* Fin du header */}
+
+
+
+      {/* Listes des activités */}
+      
+
+
+
+
     </ScrollView>
   )
 }
