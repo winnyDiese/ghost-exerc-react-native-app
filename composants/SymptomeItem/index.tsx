cb187d1b44afcde44cce94dadcda55721b13ce8b
winnyDiese <winnydiese@gmail.com>
2025-01-23 17:23:04 +0100

    lkjblkjb

--- composants/SymptomeItem/index.tsx
+++ composants/SymptomeItem/index.tsx
@@ -1,10 +1,11 @@
 
-import { View, Text, TouchableOpacity} from 'react-native'
+import { View, Text, TouchableOpacity, Image} from 'react-native'
 
 const SymptomeItem = ({item}) => {
     return (
         <TouchableOpacity>
             <View>
+                <Image source={require('../../assets/images/react-logo.png')} />
                 <Text style={{color:"white"}}>{item.libelle}</Text>
             </View>
 
