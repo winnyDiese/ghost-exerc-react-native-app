29c804d6c82ac6d5d4ec4c5ec322039b083833e1
winnyDiese <winnydiese@gmail.com>
2025-01-23 17:31:55 +0100

    lkjn

---
+++ composants/SymptomeItem/etyle.tsx
@@ -0,0 +1,16 @@
+
+import { StyleSheet } from "react-native"
+
+const styles = StyleSheet.create({
+    item: {
+        marginRight: 15,
+        flexDirection: 'row',
+        backgroundColor: 'white',
+        paddingVertical
+    },
+    itemImg: {
+        width: 30,
+        height: 30,
+        marginRight: 3
+    }
+})
--- composants/SymptomeItem/index.tsx
+++ composants/SymptomeItem/index.tsx
@@ -1,11 +1,12 @@
 
 import { View, Text, TouchableOpacity, Image} from 'react-native'
+import styles from './etyle'
 
 const SymptomeItem = ({item}) => {
     return (
         <TouchableOpacity>
-            <View>
-                <Image source={require('../../assets/images/react-logo.png')} />
+            <View style={styles.item}>
+                <Image style={styles.itemImg} source={require('../../assets/images/react-logo.png')} />
                 <Text style={{color:"white"}}>{item.libelle}</Text>
             </View>
 
